b38ad958aef051f78e59b6a05d1d16aee645ad3b
Paul <paul_araque@Pauls-MacBook-Pro.local>
2024-02-28 14:19:55 -0600

    Cambios en Unity y VideoJuegos

---
+++ Unity/VideoJuego/Assets/Script/PongScore.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongScore : MonoBehaviour
+{
+    GameController controller;
+    public int side;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.FindWithTag("GameController").GetComponent<GameController>();    
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        controller.AddPoints(side);
+    }
+}
